efbbe2e7f226bf7574800544ff69692b29781bf5
aparna999 <b.aparna.pyla@gmail.com>
2024-04-25 16:18:02 -0700

    Add the last updated timestamp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NextHoursComponent } from './components/next-hours/next-hours.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { NextFiveDaysComponent } from './components/next-five-days/next-five-days.component';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
@@ -16,8 +17,16 @@ import { NextFiveDaysComponent } from './components/next-five-days/next-five-day
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-app';
-
   cities = ['Rio de Janeiro', 'Beijing', 'Los Angeles'];
+  lastUpdatedTimestamp: string = '';
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit() {
+    this.weatherService.getLastCallTimestamp().subscribe((timestamp) => {
+      this.lastUpdatedTimestamp = timestamp;
+    });
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { WeatherData } from '../models/weather-data';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -10,26 +11,46 @@ export class WeatherService {
   //Usually, the api url and key should be accessed from environment files
   private apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
   private apiKey = '482944e26d320a80bd5e4f23b3de7d1f';
+  private lastCallTimestamp: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
 
   constructor(private http: HttpClient) {}
+
+  getLastCallTimestamp(): Observable<string> {
+    return this.lastCallTimestamp
+      .asObservable()
+      .pipe(map((timestamp) => this.formatTimestamp(timestamp)));
+  }
+
+  private formatTimestamp(timestamp: number): string {
+    const formattedDate = formatDate(timestamp, 'MMM dd HH:mm', 'en-US');
+    return formattedDate;
+  }
 
   //The free open api subscription is not providing hourly data, instead provides 3-hour Forecast for 5 days.
   //So I could display only 3 hrs forcast instead of hourly
   getHourly(city: string): Observable<WeatherData> {
+    const currentTimestamp = Date.now(); // Current timestamp in milliseconds
     const params = new HttpParams()
       .set('q', city)
       .set('appid', this.apiKey)
       .set('units', 'imperial')
       .set('cnt', '4'); //fetch data for next 4 data points
 
+    // Update lastCallTimestamp
+    this.lastCallTimestamp.next(currentTimestamp);
     return this.http.get<WeatherData>(this.apiUrl, { params });
   }
 
   get5Days(city: string): Observable<WeatherData> {
+    const currentTimestamp = Date.now(); // Current timestamp in milliseconds
     const params = new HttpParams()
       .set('q', city)
       .set('appid', this.apiKey)
       .set('units', 'imperial');
+
+    // Update lastCallTimestamp
+    this.lastCallTimestamp.next(currentTimestamp);
     return this.http.get<WeatherData>(this.apiUrl, { params });
   }
 }
